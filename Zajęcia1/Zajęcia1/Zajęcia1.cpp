51961a918dc13e178d0c8792d3a5f0b36eb63da0
Chwastu <Chwastu@LAPTOP-NGSQ8UCP>
2025-03-17 18:36:01 +0100

    po lab3

---
+++ Zajęcia1/Zajęcia1/Zajęcia1.cpp
@@ -0,0 +1,102 @@
+﻿// Zajęcia1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+void Z1a(unsigned int n) {
+	for (unsigned int i = 0; i < n; i++) {
+		cout << i % 10;
+	}
+	cout << endl;
+}
+void Z1b(unsigned int n) {
+	for (unsigned int i = 0; i < n; i++) {
+		cout << i % 2;
+	}
+	cout << endl;
+}
+void Z1d(unsigned int n) {
+	for (unsigned int i = 0; i < n *4; i++) {
+		cout << i % 4;
+	}
+	cout << endl;
+}
+void Z1e(unsigned int n) {
+	for (unsigned int i = 0; i < n*10 ; i++) {
+		cout << i % 10;
+	}
+	cout << "0123";
+	cout << endl;
+}
+void Z2d(unsigned int n) {
+	for (unsigned int i = 0; i < n; i++) {
+		for (unsigned int j = n; j > i; j--) {
+			cout << j;
+		}
+		cout << endl;
+	}
+}
+void Z2b(unsigned int n) {
+	for (unsigned int i = 1; i <=n; i++) {
+		for (unsigned int j = 1;j <= i; j++) {
+			cout << j;
+		}
+		cout << endl;
+	}
+	
+}
+void Z2a(unsigned int n) {
+	for (unsigned int i = 1; i <= n; i++) {
+		for (unsigned int j = n; j >= 1 ; j--) {
+			cout << j;
+		}
+		cout << endl;
+	}
+}
+void Z3(unsigned int n) {
+	int suma = 0;
+	for (unsigned int i = 1; i < n; i += 2) {
+		suma += i;
+	}
+	cout << suma << endl;
+}
+void Z4(int a, int b) {
+	int licznik = 0;
+	int start = (b - 1) / 9 * 9;
+
+	for (int i = start; i > a; i -= 9) {
+		cout << i << " ";
+		licznik++;
+	}
+
+	cout << "\nLiczba wielokrotności 9 w przedziale (" << a << ", " << b << ") wynosi: " << licznik << endl;
+}
+	bool czy_doskonala(unsigned int n){
+		int suma = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				suma += i;
+			}
+		}
+		return suma == n;
+	}
+	
+
+int main() {
+	unsigned int n;
+	//int suma = 0;
+	cout << "podaj n" << endl;
+	cin >> n;
+	//int a, b;
+	//cout << "Podaj liczby a i b (a ≤ b): ";
+	//cin >> a >> b;
+
+	Z1d(n);
+	//if (czy_doskonala(n)) {
+		//cout << n << " jest liczba doskonala!" << endl;
+	//}
+	//else {
+		//cout << n << " nie jest liczba doskonala." << endl;
+	//}
+	return 0;
+} 
---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,113 @@
+﻿
+
+#include <iostream>
+
+
+using namespace std;
+double accumulator = 0;
+void Wyswietl_menu() {
+	cout << "Kalulator - wybierz operacje :" << endl;
+	cout << "1. Dodawanie(+)" << endl;
+	cout << "2. Odejmowanie(-)" << endl;
+	cout << "3. Mnozenie(*)" << endl;
+	cout << "4. Dzielenie(/)" << endl;
+	cout << "5. Modulo(%)" << endl;
+	cout << "6. Reset akumulatora" << endl;
+	cout << "0. Wyjscie" << endl;
+
+}
+void Wybor_menu() {
+	int wybor;
+	cout << "Wybierz operacje: ";
+	cin >> wybor;
+	switch (wybor) {
+	case 1:
+	{
+		double a, b;
+		char useAcc;
+		cout << "Czy chcesz uzyc akumulatora? (t/n): ";
+		cin >> useAcc;
+		if (useAcc == 't') {
+			a = accumulator;
+			cout << "Pierwsza liczba(akumulator);" << a << endl;
+
+		}
+		else {
+			cout << "Podaj a: ";
+			cin >> a;
+		}
+			cout << "Podaj b: ";
+			cin >> b;
+			accumulator = a + b;
+			cout << "Wynik: " << accumulator << endl;
+			break;
+
+		}
+	case 2:
+	{
+		double a, b;
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << "Wynik: " << a - b << endl;
+		break;
+	}
+	case 3:
+	{
+		double a, b;
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << "Wynik: " << a * b << endl;
+		break;
+	}
+	case 4:
+	{
+		double a, b;
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << "Podaj b: ";
+		cin >> b;
+		if (b == 0) {
+			cout << "Nie mozna dzielic przez 0" << endl;
+		}
+		else {
+			cout << "Wynik: " << a / b << endl;
+		}
+		break;
+	}
+	case 5:
+	{
+		double a, b;
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << "Podaj b: ";
+		cin >> b;
+		if ((int)b != 0) {
+			cout << "Wynik: " << (int)a % (int)b << endl;
+		}
+		else {
+			cout << "Nie mozna dzielic przez 0" << endl;
+			break;
+		}
+	case 0:
+	{
+		cout << "Koniec programu" << endl;
+		break;
+	}
+	default:
+	{
+		cout << "Nie ma takiej opcji" << endl;
+		break;
+	}
+	}
+	}
+	}
+
+int main() {
+	Wyswietl_menu();
+	Wybor_menu();
+	return 0;
+}
